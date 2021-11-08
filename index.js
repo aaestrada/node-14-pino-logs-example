66c5f722c4bc1e4d2b824e472f30e20599304945
aaestrada <v-aaestrada@expediagroup.com>
2021-11-08 10:54:59 -0700

    set up sonic-boom to manage logs files

--- index.js
+++ index.js
@@ -3,34 +3,56 @@
 const pino = require('pino')
 const { tmpdir } = require('os')
 const { join } = require('path');
+const SonicBoom = require('sonic-boom');
 
-const file = join(tmpdir(), `pino-${process.pid}-example`);
+const logFiles = ['error', 'info']
+
+mkdirLogs(logFiles);
 
 const transport = pino.transport({
-  targets: [{
-    level: 'info',
-    target: 'pino-elasticsearch',
-    options: {
-        index: 'an-index',
-        type: 'log',
-        consistency: 'one',
-        node: 'http://localhost:9200',
-        'es-version': 6,
-        'bulk-size': 200,
-        ecs: true
-    }
-  }, {
+  targets: [
+  //   {
+  //   level: 'info',
+  //   target: 'pino-elasticsearch',
+  //   options: {
+  //       index: 'an-index',
+  //       type: 'log',
+  //       consistency: 'one',
+  //       node: 'http://localhost:9200',
+  //       'es-version': 6,
+  //       'bulk-size': 200,
+  //       ecs: true
+  //   }
+  // }, 
+  {
     level: 'info',
     target: 'pino-pretty',
     options: {
-        destination: 'logs/.log'
+        destination: 'logs/.info.log'
+    }
+  },
+  {
+    level: 'error',
+    target: 'pino-pretty',
+    options: {
+        destination: 'logs/.error.log'
     }
   }]
-})
+});
 
 const logger = pino(transport)
 
-logger.info('hello world')
+logger.info('hello world');
+
+logger.error('error example log')
 
 const child = logger.child({ a: 'property' })
 child.info('hello child!')
+
+
+
+function mkdirLogs(logFiles = []){
+  logFiles.map(
+    file => { const sonic = new SonicBoom({ dest: `logs/.${file}.log`, mkdir: true });}
+  )
+}
